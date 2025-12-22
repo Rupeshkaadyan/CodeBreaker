2b3a54e1fe65b36391b5a660a7d2bc27868dc438
Rupeshkaadyan <134736518+Rupeshkaadyan@users.noreply.github.com>
2025-12-22 23:31:33 +0530

    Initial commit: CodeBreaker project setup

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -9,14 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+       
+    
+        
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "timelapse")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("so yeah")
+           
         }
         .padding()
+        .foregroundStyle(.yellow)
+        .fontWeight(.heavy)
+        .font(.title3)
+        
     }
+        
+        
+    
 }
 
 #Preview {
